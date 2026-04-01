5b8698b3091f86f5c1d963c44544a6b83060fc34
Nebula <167588046+nebulatte@users.noreply.github.com>
2026-04-01 21:36:39 +0800

    Bubble Sort

--- ArraySort.java
+++ ArraySort.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class arraySort {
+public class ArraySort {
     public static void main(String[] args) {
         
         Scanner scan = new Scanner(System.in);
